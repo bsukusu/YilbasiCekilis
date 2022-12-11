81d5ee1e6f14b08693dea9947bb4bcac32d13502
bsukusu <bsukusu@TEV157>
2022-12-11 22:47:54 +0300

    Proje dosyası ekle.

---
+++ YilbasiCekilis/Homapage.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace YilbasiCekilis
+{
+    partial class Homapage
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Homapage));
+            this.NoelpictureBox = new System.Windows.Forms.PictureBox();
+            this.isimlabel = new System.Windows.Forms.Label();
+            this.isimlercomboBox = new System.Windows.Forms.ComboBox();
+            this.isimcekbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NoelpictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NoelpictureBox
+            // 
+            this.NoelpictureBox.Image = ((System.Drawing.Image)(resources.GetObject("NoelpictureBox.Image")));
+            this.NoelpictureBox.Location = new System.Drawing.Point(181, 21);
+            this.NoelpictureBox.Name = "NoelpictureBox";
+            this.NoelpictureBox.Size = new System.Drawing.Size(111, 84);
+            this.NoelpictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.NoelpictureBox.TabIndex = 0;
+            this.NoelpictureBox.TabStop = false;
+            this.NoelpictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // isimlabel
+            // 
+            this.isimlabel.AutoSize = true;
+            this.isimlabel.Location = new System.Drawing.Point(62, 161);
+            this.isimlabel.Name = "isimlabel";
+            this.isimlabel.Size = new System.Drawing.Size(44, 13);
+            this.isimlabel.TabIndex = 1;
+            this.isimlabel.Text = "İsminiz: ";
+            // 
+            // isimlercomboBox
+            // 
+            this.isimlercomboBox.FormattingEnabled = true;
+            this.isimlercomboBox.Location = new System.Drawing.Point(181, 153);
+            this.isimlercomboBox.Name = "isimlercomboBox";
+            this.isimlercomboBox.Size = new System.Drawing.Size(97, 21);
+            this.isimlercomboBox.TabIndex = 2;
+            // 
+            // isimcekbutton
+            // 
+            this.isimcekbutton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("isimcekbutton.BackgroundImage")));
+            this.isimcekbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.isimcekbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.isimcekbutton.Location = new System.Drawing.Point(115, 204);
+            this.isimcekbutton.Name = "isimcekbutton";
+            this.isimcekbutton.Size = new System.Drawing.Size(105, 92);
+            this.isimcekbutton.TabIndex = 3;
+            this.isimcekbutton.Text = "Çekilişi Yap";
+            this.isimcekbutton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.isimcekbutton.UseVisualStyleBackColor = true;
+            this.isimcekbutton.Click += new System.EventHandler(this.isimcekbutton_Click);
+            // 
+            // Homapage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 500);
+            this.Controls.Add(this.isimcekbutton);
+            this.Controls.Add(this.isimlercomboBox);
+            this.Controls.Add(this.isimlabel);
+            this.Controls.Add(this.NoelpictureBox);
+            this.Name = "Homapage";
+            this.Text = "Yılbaşı Çekilişi";
+            this.Load += new System.EventHandler(this.Homapage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NoelpictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox NoelpictureBox;
+        private System.Windows.Forms.Label isimlabel;
+        private System.Windows.Forms.ComboBox isimlercomboBox;
+        private System.Windows.Forms.Button isimcekbutton;
+    }
+}
+
---
+++ YilbasiCekilis/Homapage.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YilbasiCekilis
+{
+    public partial class Homapage : MetroFramework.Forms.MetroForm
+    {
+        public string[] List_Tam = Properties.Resources.YilbasiCekilisitext.Replace("\r", "").Split('\n');
+        public string[] List_Kalan;
+        public Homapage()
+        {
+            InitializeComponent();
+        }
+        
+        private void Homapage_Load(object sender, EventArgs e)
+        {
+            List_Kalan = List_Tam;
+            UpdateComboList();
+        }
+
+        public void UpdateComboList() {
+            isimlercomboBox.Items.Clear();
+            for (int i = 0; i < List_Kalan.Count(); i++)
+            {
+                isimlercomboBox.Items.Add(List_Kalan[i]);
+            }
+        }
+
+        private void isimcekbutton_Click(object sender, EventArgs e)
+        {
+            CekilisYap();
+        }
+
+        public void CekilisYap() {
+
+            //MessageBox.Show(isimlercomboBox.SelectedItem.ToString());
+            if (isimlercomboBox.SelectedIndex == -1) { MessageBox.Show("seçim yapmadınız !"); }
+            else
+            {
+                if (isimlercomboBox.SelectedItem.ToString() != "")
+                {
+                    if (List_Kalan.Count() == 0)
+                    {
+                        MessageBox.Show("Çekiliş Bitti");
+                        isimcekbutton.Enabled = false;
+                    }
+                    else
+                    {
+                        #region "Seçim Yap"
+                            string[] List_Kalan2 = new string[0];
+                            for (int i = 0; i < List_Kalan.Count(); i++)
+                            {
+                                if (List_Kalan[i] != isimlercomboBox.SelectedItem.ToString() ) {
+                                    List_Kalan2 = new string[List_Kalan2.Count()+1];
+                                    List_Kalan2[List_Kalan2.Count()] = List_Kalan[i];  
+                                }
+                            }
+                            Random rastgeleNumara = new Random();
+                            int secilenRastgeleNumara = rastgeleNumara.Next(0, List_Kalan2.Count());
+                        #endregion
+
+                        MessageBox.Show(List_Kalan2[secilenRastgeleNumara]);
+
+                        #region "ComboBox'tan Düşme"
+                            isimlercomboBox.Items.Remove(isimlercomboBox.SelectedIndex);
+                        #endregion
+
+
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("isminizi giriniz");
+                }
+            }
+
+
+        }
+    }
+}
